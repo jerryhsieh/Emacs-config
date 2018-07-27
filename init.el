32ada7a6088a27c21eeb7b02e8e56a2c7e99b9a1
Jerry Hsieh <jerryhsieh01@gmail.com>
2018-07-27 15:19:34 +0800

    add company-mode

--- init.el
+++ init.el
@@ -117,4 +117,43 @@
   (use-package yasnippet-snippets :ensure t)
   )
 
+;;
+;; company
+;;
+(use-package company
+  :ensure t
+  :config
+  (global-company-mode t)
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 3)
+  (setq company-backends
+        '((company-files
+           company-keywords
+           company-capf
+           company-yasnippet
+           )
+          (company-abbrev company-dabbrev))))
+
+(add-hook 'emacs-lisp-mode-hook (lambda () (set (make-local-variable 'company-backends) '(company-elisp))))
+
+
+;;
+;; change C-n C-p
+;;
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous)
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil))
+
+
+;;
+;; change company complete common
+;;
+(advice-add 'company-complete-common :before (lambda () (setq my-company-point (point))))
+(advice-add 'company-complete-common :after (lambda () (when (equal my-company-point (point))
+                                                         (yas-expand))))
+
+
+
 
