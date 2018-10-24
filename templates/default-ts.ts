56933c97297b3f86ac91c55ac2b35340adb2551c
Jerry Hsieh <jerryhsieh01@gmail.com>
2018-10-24 18:55:44 +0800

    add js html templates

---
+++ templates/default-ts.ts
@@ -0,0 +1,7 @@
+//
+//
+// File name : `(file-name-nondirectory (buffer-file-name))`
+// Author: `user-full-name` @ `(format-time-string "%Y-%m-%d")`
+// Copyright © `(format-time-string "%Y")`, `user-full-name`, all rights reserved.
+// 
+// 
